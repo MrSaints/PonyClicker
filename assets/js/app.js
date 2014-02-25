bcea62c2d2dd9217c6f67f68a2ba324037766be7
MrSaints <ian@fyianlai.com>
2014-02-25 07:47:25 +0000

    New layout; added upgrades

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,23 +27,64 @@ angular.module('ngDebounce', []).factory('$debounce', function ($timeout, $q) {
 
 var $MLP = angular.module('ponyClicker', ['ngDebounce']);
 
-$MLP.controller('indexCtrl', ['$scope', '$debounce', function ($scope, $debounce) {
+$MLP.controller('indexCtrl', ['$scope', '$debounce', '$interval', function ($scope, $debounce, $interval) {
     var $this = this;
 
+    /*
+     * Count (Clicks)
+     */
     $scope.count = Number(localStorage.getItem('count')) || 0;
     $scope.incrementCount = function () {
-        return $scope.count += 1;
+        $scope.count += 1;
     }
 
-    var saveCount = function (newValue, oldValue) {
+    /*
+     * Count (Upgrades)
+     */
+    $this.incrementUpCount = function () {
+        var upCount = 0;
+        angular.forEach($scope.upgrades, function(value, key) {
+           upCount += (value.rate * value.total);
+        });
+        $scope.count += upCount;
+    }
+    $interval($this.incrementUpCount, 1000);
+
+    /*
+     * Upgrades
+     */
+    $scope.upgrades = JSON.parse(localStorage.getItem('upgrades')) || { 
+        autoclicker: {
+            cost: 20,
+            multiplier: 1.1,
+            rate: 0.1,
+            total: 0
+        }
+    };
+
+    // Autoclicker
+    $scope.buyAutoclicker = function () {
+        if ($scope.count < $scope.upgrades.autoclicker.cost)
+            return;
+
+        $scope.count -= $scope.upgrades.autoclicker.cost;
+        $scope.upgrades.autoclicker.total += 1;
+        $scope.upgrades.autoclicker.cost *= $scope.upgrades.autoclicker.multiplier;
+    }
+
+    /*
+     * Saving
+     */
+    $this.save = function (newValue, oldValue) {
         console.log('Saving...');
         localStorage.setItem('count', newValue);
+        localStorage.setItem('upgrades', JSON.stringify($scope.upgrades));
     }
 
-    $scope.$watch('count', $debounce(saveCount, 500), true);
+    $scope.$watch('count', $debounce($this.save, 3000), true);
 
     window.onbeforeunload = function() {
         console.log('Forcing save...');
-        saveCount($scope.count);
+        $this.save($scope.count);
     };
 }]);
