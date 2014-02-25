17ecd91e90c7d92702c4589b4fbff83d3314130d
MrSaints <ian@fyianlai.com>
2014-02-25 05:16:13 +0000

    Angular implementation

--- script.js
+++
@@ -1,8 +0,0 @@
-count = 0;
-function init() {
-    document.getElementById("pony").addEventListener("click", click_pony, false);
-}
-function click_pony() {
-    ++count;
-    document.getElementById("count").textContent = count;
-}
